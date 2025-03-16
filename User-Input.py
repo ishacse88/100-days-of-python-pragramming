b877d8728fd4c8d6063d46a47026d3e0ff488789
ishacse88 <ishakumari2927@gmail.com>
2025-03-16 21:25:01 +0530

    Add files via upload

---
+++ User-Input.py
@@ -0,0 +1,16 @@
+input("enter email:")
+name=input("enter your name:")
+print(name)
+
+age=int(input("enter your age:"))
+print(age)
+
+
+first_num = int(input("Enter first number: "))
+second_num = int(input("Enter second number: "))
+result = first_num + second_num
+print(result)
+print( first_num + second_num)
+
+print(type(name))
+print(type(first_num))
