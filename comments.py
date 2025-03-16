4e0de9860536f379607997d2d9b49e1444cbce4f
ishacse88 <ishakumari2927@gmail.com>
2025-03-16 20:56:58 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,17 @@
+#single line comments
+#print(a+b)
+
+""""
+multi line comments
+dict={
+    'name':"xyz",'age':23,
+}
+print(dict)"
+"""
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("hello")
