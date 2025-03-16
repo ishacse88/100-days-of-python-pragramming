5e81bccd255ab18ddd011ce2e576d5ca0fdb42a4
ishacse88 <ishakumari2927@gmail.com>
2025-03-16 21:10:52 +0530

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,12 @@
+"""
+Python has a set of keywords that are reserved words
+that cannot be used as variable names, function names, 
+or any other identifiers
+"""
+
+
+#and=23 cannot be used as variable name  ("and" is A logical operator)
+#for="isha"  
+#if=2
+#with="abc"
+
