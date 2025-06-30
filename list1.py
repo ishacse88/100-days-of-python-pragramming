633f04c6f82588f752cf1d36126c65f14c5f6203
ishacse88 <ishakumari2927@gmail.com>
2025-06-30 12:25:27 +0530

    Add files via upload

--- list1.py
+++ list1.py
@@ -1,9 +1,43 @@
+list=[22,89,100,34]
+print(max(list))
+print(min(list))
 
-n=int(input("enter no. of people for rating:"))
-movie_rating=[]
-for i in range(n):
- rating=int(input("enter movie rating as per your choise in btwn 0 to 10:")) 
- movie_rating.append(rating)
-print("original ratings:",movie_rating)
+list.reverse()
+print("reversed list",list)
+
+list.sort(reverse=True)
+print("reversed list:", list)
+
+
+a=sorted(list)
+b=list.copy()
+if sorted(list)==list.copy():
+    print("list is sorted")
+else:
+    print("list is not sotred")
+
+numbers = [22, 89, 100, 34]
+
+# Remove duplicates (important in case of repeated numbers)
+unique_numbers = list(set(numbers))
+
+# Step 1: Find the largest and smallest
+largest = max(unique_numbers)
+smallest = min(unique_numbers)
+
+# Step 2: Remove them from the list
+unique_numbers.remove(largest)
+unique_numbers.remove(smallest)
+
+# Step 3: Find second largest and second smallest
+second_largest = max(unique_numbers)
+second_smallest = min(unique_numbers)
+
+print("Second Largest:", second_largest)
+print("Second Smallest:", second_smallest)
+
+
+
+
 
 
